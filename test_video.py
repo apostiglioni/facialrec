41b618879aa249510ab47349dffa7d052cff5e83
Andres Postiglioni <apostiglioni@gmail.com>
2016-09-22 00:14:00 +0000

    working

--- test_video.py
+++ test_video.py
@@ -1,10 +1,10 @@
 # import the necessary packages
+from itertools import chain
+from multiprocessing import cpu_count, Pool
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-
-
 
 def capture(camera, rawCapture):
     # capture frames from the camera
@@ -14,15 +14,15 @@ def capture(camera, rawCapture):
         frame = frame.array
         yield frame
 
-def find_faces(frame, classifier, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def find_faces(frame, definition, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)):
+    classifier = cv2.CascadeClassifier(definition)
 
     faces = classifier.detectMultiScale(
-        gray,
-        scaleFactor=scaleFactor,
-        minNeighbors=minNeighbors,
-        minSize=minSize,
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        frame,
+        scaleFactor = scaleFactor,
+        minNeighbors = minNeighbors,
+        minSize = minSize,
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
     return faces
@@ -39,37 +39,36 @@ def show_frame(frame):
     # show the frame
     cv2.imshow("Frame", frame)
 
-
 def run():
     # initialize the camera and grab a reference to the raw camera capture
-    
     resolution = (320, 240)
-    
+
     camera = PiCamera()
     camera.resolution = resolution
     camera.framerate = 32
     rawCapture = PiRGBArray(camera, size=resolution)
-     
+
     # allow the camera to warmup
     time.sleep(0.1)
 
     definitions = ['classifiers/haarcascade_frontalface_default.xml', 'classifiers/haarcascade_profileface.xml', 'classifiers/lbpcascade_profileface.xml']
 
-    classifiers = [cv2.CascadeClassifier(definition) for definition in definitions]
+    pool = Pool(processes=cpu_count())
     for frame in capture(camera, rawCapture):
-#       rimg=frame.copy()
-#       rimg=cv2.flip(frame,1)
-        for classifier in classifiers:
-            faces = find_faces(frame, classifier)
-            log(faces)
+        processes = []
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        processes = map(lambda definition: pool.apply_async(find_faces, (gray, definition)), definitions)
+        faces_list = map(lambda response: response.get(), processes)
+        log(faces_list)
+        for faces in faces_list:
             highlight_faces(frame, faces)
 
         show_frame(frame)
         key = cv2.waitKey(1) & 0xFF
- 
+
         # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
-     
+
         # if the `q` key was pressed, break from the loop
         if key == ord("q"):
             break
